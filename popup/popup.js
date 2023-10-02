097e205aa0bfbbf1f4cb91ab1bfb9dcf0bfbe8d8
beomkwan.choi <3031011@naver.com>
2023-10-02 22:49:56 +0900

    ✨ feat: 유저와의 상호작용을 위한 DOM 제어 JS 추가

---
+++ popup/popup.js
@@ -0,0 +1,70 @@
+// dom
+const $fontSizeSection = document.querySelector(".font-size-wrap");
+const $fontSelectButton = document.querySelector(".font-select-button");
+const $fontBoldCheckBox = document.getElementById("font-bold");
+const $fontBoldToggleButton = document.querySelector(".toggle");
+const $fontOptionsDropDown = document.querySelector(".font-options");
+const $fontColors = document.querySelector(".font-colors");
+const $backgroundColors = document.querySelector(".background-colors");
+const FONT_SIZE_MAX = 64;
+const FONT_SIZE_MIN = 10;
+
+const increaseFontSize = ($fontSizeSpan) => {
+  $fontSizeSpan.textContent = 1 * $fontSizeSpan.textContent + 2;
+};
+
+const dicreaseFontSize = ($fontSizeSpan) => {
+  $fontSizeSpan.textContent -= 2;
+};
+
+const fontSizeTextHandler = (e) => {
+  const $fontSizeSpan = e.currentTarget.querySelector(".font-size");
+  const $plusButton = e.currentTarget.querySelector(".plus");
+  const $minusButton = e.currentTarget.querySelector(".minus");
+
+  switch (e.target.className) {
+    case "plus":
+      increaseFontSize($fontSizeSpan);
+      break;
+    case "minus":
+      dicreaseFontSize($fontSizeSpan);
+      break;
+  }
+
+  $plusButton.disabled = $fontSizeSpan.textContent == FONT_SIZE_MAX;
+  $minusButton.disabled = $fontSizeSpan.textContent == FONT_SIZE_MIN;
+};
+
+const fontDropDownClose = (e) => {
+  $fontSelectButton.classList.remove("active");
+};
+
+const fontDropDownToggle = () => {
+  $fontSelectButton.classList.toggle("active");
+};
+
+const assignFontOption = (e) => {
+  if (e.target.type === "button") {
+    $fontSelectButton.querySelector(".selected-font").textContent = e.target.textContent;
+    fontDropDownClose();
+  }
+};
+
+const colorHandler = (e) => {
+  if (e.target.classList.contains("custom")) {
+    e.currentTarget.querySelector("input[type='color']").click();
+  }
+};
+
+const keyboardItemClick = (e, item) => {
+  if (e.key === " " || e.key === "Enter") {
+    item.click();
+  }
+};
+
+$fontSelectButton.addEventListener("click", fontDropDownToggle);
+$fontSizeSection.addEventListener("click", fontSizeTextHandler);
+$fontOptionsDropDown.addEventListener("click", assignFontOption);
+$fontColors.addEventListener("click", (e) => colorHandler(e));
+$backgroundColors.addEventListener("click", (e) => colorHandler(e));
+$fontBoldToggleButton.addEventListener("keydown", (e) => keyboardItemClick(e, $fontBoldCheckBox));
